b9327a97b9a85e1820e7eaad38e45ce25c180c27
kedharnadhG <g.kedharnadh098@gmail.com>
2025-06-08 09:03:47 +0530

    tailwind + daisyUI + Navbar added

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+// tailwind.config.js
+import daisyui from 'daisyui';
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    './index.html',
+    './src/**/*.{js,ts,jsx,tsx}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [daisyui],
+  daisyui: {
+    themes: ["dracula", 'light', 'dark', 'cupcake'],
+    darkTheme: "dracula",
+  },
+};
